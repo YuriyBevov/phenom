14dfc1f29e18f98b1faa9988c349fb2671475553
Yuriy Bevov <yuriybevov@gmail.com>
2026-07-02 12:43:10 +0300

    Добавил тиззеры

--- local/templates/littleweb/components/bitrix/news.list/tizzers/template.php
+++ local/templates/littleweb/components/bitrix/news.list/tizzers/template.php
@@ -7,12 +7,10 @@ if ($arResult["ITEMS"]): ?>
 				$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 				$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 			?>
-				<? if ($arItem["PROPERTIES"]["VALUE"]["VALUE"] && $arItem["PROPERTIES"]["NAME"]["VALUE"]): ?>
-					<div class="tizzers__item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-						<strong><?= $arItem["PROPERTIES"]["VALUE"]["VALUE"] ?></strong>
-						<span><?= $arItem["PROPERTIES"]["NAME"]["VALUE"] ?></span>
-					</div>
-				<? endif; ?>
+				<div class="tizzers__item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+					<strong><?= $arItem["NAME"] ?></strong>
+					<span><?= $arItem["PREVIEW_TEXT"] ?></span>
+				</div>
 			<? endforeach; ?>
 		</div>
 	</div>
